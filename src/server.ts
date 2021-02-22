fb98911efc3f076e3dec729aebd918046ae61a92
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-02-22 19:05:53 -0300

    Feat: create server and add routes

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const server = express();
+
+server.get('/', (request, response) => response.json({ message: 'Hello World' }));
+
+server.post('/', (request, response) => response.json({ message: 'Os dados foram salvos com sucesso!' }));
+
+server.listen(3030, () => console.log('Server is running'));
