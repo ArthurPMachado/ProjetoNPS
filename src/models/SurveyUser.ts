31487a677b2c0c4c4a5a80016cd0802a328df55f
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-02-25 18:45:11 -0300

    Feat: add ManyToOne relations in SurveyUser

--- src/models/SurveyUser.ts
+++ src/models/SurveyUser.ts
@@ -1,7 +1,10 @@
 import {
-  Column, CreateDateColumn, Entity, PrimaryColumn,
+  Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn,
 } from 'typeorm';
 import { v4 as uuid } from 'uuid';
+
+import User from './User';
+import Survey from './Survey';
 
 @Entity('surveys_users')
 class SurveyUser {
@@ -11,8 +14,16 @@ class SurveyUser {
   @Column()
   user_id: string;
 
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User
+
   @Column()
   survey_id: string;
+
+  @ManyToOne(() => Survey)
+  @JoinColumn({ name: 'survey_id' })
+  survey: Survey
 
   @Column()
   value: number;
