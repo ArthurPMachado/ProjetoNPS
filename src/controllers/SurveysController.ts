270241142c98b54494b2b5bd699ccbbc6cfd4301
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-02-26 16:28:17 -0300

    Feat: add yup validation

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import * as yup from 'yup';
 import SurveysRepository from '../repositories/SurveysRepository';
 
 class SurveysController {
   async create(request: Request, response: Response) {
     const { title, description } = request.body;
+
+    const schema = yup.object().shape({
+      titel: yup.string().required(),
+      description: yup.string().email().required(),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
 
     const surveysRepository = getCustomRepository(SurveysRepository);
 
